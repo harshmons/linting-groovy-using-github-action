7c324e736fca9bc54d449dded7ee1471f5ddccf4
Harsh Verma <harshverma04111989@gmail.com>
2023-10-08 13:07:15 +0530

    Update sample.groovy

--- src/sample.groovy
+++ src/sample.groovy
@@ -1,7 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
-          int x = 104;
-            System.out.println(x);
-        //x = "sample";
+        int x = 104;
+        System.out.println(x);
     }
 }
